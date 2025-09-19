32fd0ef40bd559d2969e21300cffe8b86510d004
Albert Bakhitov <bakhitov-ai@zvuk.com>
2025-09-20 01:28:58 +0400

    Initial Commit

---
+++ DoctorsAppointmentAppProjectTests/DoctorsAppointmentAppProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsAppointmentAppProjectTests.swift
+//  DoctorsAppointmentAppProjectTests
+//
+//  Created by Альберт Бахитов on 20.09.2025.
+//
+
+import Testing
+@testable import DoctorsAppointmentAppProject
+
+struct DoctorsAppointmentAppProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
