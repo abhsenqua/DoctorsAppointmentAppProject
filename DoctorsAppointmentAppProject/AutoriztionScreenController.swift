653d28458a726e2425241d64d421c203f98fe757
Albert Bakhitov <bakhitov-ai@zvuk.com>
2025-09-20 02:06:08 +0400

    Создал контроллер экрана авторизации, добавил модель с переменными

---
+++ DoctorsAppointmentAppProject/AutoriztionScreenController.swift
@@ -0,0 +1,13 @@
+//
+//  AutoriztionScreenController.swift
+//  DoctorsAppointmentAppProject
+//
+//  Created by Альберт Бахитов on 20.09.2025.
+//
+
+import UIKit
+
+class AutoriztionScreenController: UIViewController {
+    
+    
+}
